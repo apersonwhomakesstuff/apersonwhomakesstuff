2fca62eeddbb8cf79887e3036bd05f148043f70e
apersonwhomakesstuff <137462576+apersonwhomakesstuff@users.noreply.github.com>
2025-07-23 20:50:07 -0500

    Update cc.lua

--- cc.lua
+++ cc.lua
@@ -2,14 +2,15 @@ local sin, cos, floor, max = math.sin, math.cos, math.floor, math.max
 
 local function main()
     local A, B = 0, 0
-    local z = {}
-    local b = {}
-
-    term.clear()
 
     while true do
-        for i = 0, 1760 do z[i] = 0.0 end
-        for i = 0, 1760 do b[i] = " " end
+        local termWidth, termHeight = term.getSize()
+        local bufferSize = termWidth * termHeight
+        local z = {}
+        local b = {}
+
+        for i = 0, bufferSize - 1 do z[i] = 0.0 end
+        for i = 0, bufferSize - 1 do b[i] = " " end
 
         for j = 0, 6.28, 0.07 do
             for i = 0, 6.28, 0.07 do
@@ -25,15 +26,15 @@ local function main()
                 local sinB = sin(B)
                 local t = sini * cosj2 * cosA - sinj * sinA
 
-                local x = floor(40 + 30 * mess * (cosi * cosj2 * cosB - t * sinB))
-                local y = floor(12 + 15 * mess * (cosi * cosj2 * sinB + t * cosB))
-                local o = floor(x + 80 * y)
+                local x = floor(termWidth / 2 + (termWidth / 2.7) * mess * (cosi * cosj2 * cosB - t * sinB))
+                local y = floor(termHeight / 2 + (termHeight / 2.2) * mess * (cosi * cosj2 * sinB + t * cosB))
+                local o = floor(x + termWidth * y)
                 local N = floor(
                     8 *
                     ((sinj * sinA - sini * cosj * cosA) * cosB - sini * cosj * sinA - sinj * cosA - cosi * cosj * sinB)
                 )
 
-                if y < 22 and y > 0 and x > 0 and x < 80 and mess > z[o] then
+                if y < termHeight and y > 0 and x > 0 and x < termWidth and mess > z[o] then
                     z[o] = mess
                     local n = max(N, 1)
                     b[o] = (".,-~:;=!*#$@"):sub(n, n)
@@ -42,13 +43,13 @@ local function main()
         end
 
         term.setCursorPos(1, 1)
-        local line = ""
-        for k = 0, 1760 do
-            if k % 80 == 0 and k ~= 0 then
-                print(line)
-                line = ""
+        for y = 0, termHeight - 1 do
+            local line = ""
+            for x = 0, termWidth - 1 do
+                local k = x + termWidth * y
+                line = line .. (b[k] or " ")
             end
-            line = line .. (b[k] or " ")
+            print(line)
         end
 
         A = A + 0.04
